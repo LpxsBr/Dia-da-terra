c19692bfecaf78005586a45806a867bb0d3289e5
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-06-01 13:21:23 +0000

    Initial commit

---
+++ Js/script.js
@@ -0,0 +1,14 @@
+// funções para mudar imagens através do ID
+
+
+function funcPlant(){
+  document.getElementById('treeImg').src=`/image/pixil-frame-0 (2).png`;
+  }
+
+function funcCresc(){
+  document.getElementById('treeImg').src=`/image/pixil-frame-0 (9).png`;
+  }
+
+function funcReset(){
+  document.getElementById('treeImg').src=`/image/pixil-frame-0 (1).png`;
+  }
