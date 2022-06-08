e1b7b4842dff814377f5f7e90b98bf2d0dae1f69
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-06-08 10:38:09 -0300

    updating anything of my code :)

--- Js/script.js
+++ Js/script.js
@@ -1,14 +1,17 @@
-// funções para mudar imagens através do ID
 
+//functions to change images using id
 
+// this function is used to plant the small tree
 function funcPlant(){
   document.getElementById('treeImg').src=`/image/pixil-frame-0 (2).png`;
   }
 
+// this function is used to show the adult tree 
 function funcCresc(){
   document.getElementById('treeImg').src=`/image/pixil-frame-0 (9).png`;
   }
 
+// this function is used to reset the image
 function funcReset(){
   document.getElementById('treeImg').src=`/image/pixil-frame-0 (1).png`;
   }
